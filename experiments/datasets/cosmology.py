b96aaf8663595f04c25347a868579ee07f4afbf8
Gerben Koopman <gerbennkoopman@gmail.com>
2025-05-19 17:47:48 +0200

    Added geo_erwin datatype

--- experiments/datasets/cosmology.py
+++ experiments/datasets/cosmology.py
@@ -15,9 +15,9 @@ MEAN_HALOS_DICT = {
     "x": 499.91877075908684,
     "y": 500.0947802559321,
     "z": 499.964508664328,
-    "Jx": 212560050888254.06,
-    "Jy": 349712732356652.25,
-    "Jz": -100259775332585.12,
+    "J_x": 212560050888254.06,
+    "J_y": 349712732356652.25,
+    "J_z": -100259775332585.12,
     "v_x": -0.0512854365234889,
     "v_y": -0.01263126442198149,
     "v_z": -0.06458034372345466,
@@ -28,9 +28,9 @@ STD_HALOS_DICT = {
     "x": 288.71092533309235,
     "y": 288.7525818573022,
     "z": 288.70234893905575,
-    "Jx": 2.4294356933448945e18,
-    "Jy": 2.3490019110577966e18,
-    "Jz": 2.406422979830857e18,
+    "J_x": 2.4294356933448945e18,
+    "J_y": 2.3490019110577966e18,
+    "J_z": 2.406422979830857e18,
     "v_x": 344.0231468131901,
     "v_y": 343.9333673335964,
     "v_z": 344.071876710777,
@@ -51,20 +51,64 @@ STD_PARAMS_DICT = {
     "n_s": 0.115482554,
     "sigma_8": 0.11545073,
 }
-MEAN_TPCF_VEC = [1.47385902e+01, 4.52754450e+00, 1.89688166e+00, 1.00795493e+00,
-                6.09400184e-01, 3.98518764e-01, 2.79545049e-01, 2.01358601e-01,
-                1.53487009e-01, 1.18745081e-01, 9.51346027e-02, 7.83494908e-02,
-                6.92183650e-02, 6.41181254e-02, 6.05992822e-02, 5.77399258e-02,
-                5.27855615e-02, 4.64777462e-02, 3.97492901e-02, 3.17941626e-02,
-                2.49663476e-02, 1.92553030e-02, 1.28971533e-02, 9.48586955e-03]
-STD_TPCF_VEC = [8.37356624, 2.36190046, 1.15493691, 0.73567994, 0.52609708, 0.40239359,
-                0.32893873, 0.27772011, 0.24173466, 0.21431925, 0.19276616, 0.17816693,
-                0.16773013, 0.15968612, 0.15186733, 0.14234885, 0.13153203, 0.11954234,
-                0.10549666, 0.09024256, 0.07655078, 0.06350282, 0.05210615, 0.0426435]
+MEAN_TPCF_VEC = [
+    1.47385902e01,
+    4.52754450e00,
+    1.89688166e00,
+    1.00795493e00,
+    6.09400184e-01,
+    3.98518764e-01,
+    2.79545049e-01,
+    2.01358601e-01,
+    1.53487009e-01,
+    1.18745081e-01,
+    9.51346027e-02,
+    7.83494908e-02,
+    6.92183650e-02,
+    6.41181254e-02,
+    6.05992822e-02,
+    5.77399258e-02,
+    5.27855615e-02,
+    4.64777462e-02,
+    3.97492901e-02,
+    3.17941626e-02,
+    2.49663476e-02,
+    1.92553030e-02,
+    1.28971533e-02,
+    9.48586955e-03,
+]
+STD_TPCF_VEC = [
+    8.37356624,
+    2.36190046,
+    1.15493691,
+    0.73567994,
+    0.52609708,
+    0.40239359,
+    0.32893873,
+    0.27772011,
+    0.24173466,
+    0.21431925,
+    0.19276616,
+    0.17816693,
+    0.16773013,
+    0.15968612,
+    0.15186733,
+    0.14234885,
+    0.13153203,
+    0.11954234,
+    0.10549666,
+    0.09024256,
+    0.07655078,
+    0.06350282,
+    0.05210615,
+    0.0426435,
+]
 
 
 def num_nodes_to_batch_idx(num_nodes):
-    return torch.arange(len(num_nodes)).to(num_nodes.device).repeat_interleave(num_nodes)
+    return (
+        torch.arange(len(num_nodes)).to(num_nodes.device).repeat_interleave(num_nodes)
+    )
 
 
 def _parse_function(
@@ -81,12 +125,18 @@ def _parse_function(
     parsed_params = tf.io.parse_single_example(proto, keys_to_params)
 
     # Convert each feature from a serialized string to a tensor and store in a list
-    feature_tensors = [tf.io.parse_tensor(parsed_features[k], out_type=tf.float32) for k in features]
-    param_tensors = [tf.io.parse_tensor(parsed_params[k], out_type=tf.float32) for k in params]
+    feature_tensors = [
+        tf.io.parse_tensor(parsed_features[k], out_type=tf.float32) for k in features
+    ]
+    param_tensors = [
+        tf.io.parse_tensor(parsed_params[k], out_type=tf.float32) for k in params
+    ]
 
     # Stack the feature tensors to create a single tensor
     # Each tensor must have the same shape
-    stacked_features = tf.stack(feature_tensors, axis=1)  # Creates a [num_points, num_features] tensor
+    stacked_features = tf.stack(
+        feature_tensors, axis=1
+    )  # Creates a [num_points, num_features] tensor
     stacked_params = tf.stack(param_tensors, axis=0)  # Creates a [num_params] tensor
 
     if include_tpcf:
@@ -120,7 +170,11 @@ def get_halo_dataset(
         num_total = dataset.reduce(0, lambda x, _: x + 1).numpy()
         dataset = dataset.take(num_total)
 
-    dataset = dataset.map(partial(_parse_function, features=features, params=params, include_tpcf=include_tpcf))
+    dataset = dataset.map(
+        partial(
+            _parse_function, features=features, params=params, include_tpcf=include_tpcf
+        )
+    )
 
     # Get mean and std as tf arrays
     mean = tf.constant([MEAN_HALOS_DICT[f] for f in features], dtype=tf.float32)
@@ -132,9 +186,17 @@ def get_halo_dataset(
 
     if standardize:
         if include_tpcf:
-            dataset = dataset.map(lambda x, p, t: ((x - mean) / std, (p - mean_params) / std_params, (t - mean_tpcf) / std_tpcf))
+            dataset = dataset.map(
+                lambda x, p, t: (
+                    (x - mean) / std,
+                    (p - mean_params) / std_params,
+                    (t - mean_tpcf) / std_tpcf,
+                )
+            )
         else:
-            dataset = dataset.map(lambda x, p: ((x - mean) / std, (p - mean_params) / std_params))
+            dataset = dataset.map(
+                lambda x, p: ((x - mean) / std, (p - mean_params) / std_params)
+            )
 
     if batch_size is None:
         batch_size = num_total
@@ -161,7 +223,18 @@ class CosmologyDataset(Dataset):
             params = ["Omega_m", "sigma_8"]
 
         elif task == "geo_erwin":
-            features = ["x", "y", "z", "v_x", "v_y", "v_z", "J_x", "J_y", "J_z", "M200c"]
+            features = [
+                "x",
+                "y",
+                "z",
+                "v_x",
+                "v_y",
+                "v_z",
+                "J_x",
+                "J_y",
+                "J_z",
+                "M200c",
+            ]
             params = ["Omega_m", "Omega_b", "h", "n_s", "sigma_8"]
 
         dataset, _ = get_halo_dataset(
@@ -191,14 +264,25 @@ class CosmologyDataset(Dataset):
                 target.append(target_.numpy())
                 if i == num_data:
                     break
-        else:
+        elif self.task == "node":
             pos, target = [], []
             for i, (x, _) in enumerate(tqdm(dataset)):
                 pos.append(x[..., :3].numpy())
                 target.append(x[..., 3:].numpy())
                 if i == num_data:
                     break
-        return [torch.from_numpy(x).squeeze() for x in pos], [torch.from_numpy(x).squeeze() for x in target]
+
+        elif self.task == "geo_erwin":
+            pos, target = [], []
+            for i, (x, p) in enumerate(tqdm(dataset)):
+                pos.append(x.numpy())
+                target.append(p.numpy())
+                if i == num_data:
+                    break
+
+        return [torch.from_numpy(x).squeeze() for x in pos], [
+            torch.from_numpy(x).squeeze() for x in target
+        ]
 
     def __len__(self):
         return self.num_samples
@@ -217,5 +301,7 @@ class CosmologyDataset(Dataset):
         num_nodes = torch.tensor([d["pos"].shape[0] for d in batch])
         batch = {k: torch.cat([d[k] for d in batch], dim=0) for k in batch[0].keys()}
         batch["batch_idx"] = num_nodes_to_batch_idx(num_nodes)
-        batch["edge_index"] = knn_graph(batch["pos"], k=self.knn, batch=batch["batch_idx"])
+        batch["edge_index"] = knn_graph(
+            batch["pos"], k=self.knn, batch=batch["batch_idx"]
+        )
         return batch
