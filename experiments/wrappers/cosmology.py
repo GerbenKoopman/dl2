d8e387b801df7dee9e9e71ec4c9d64da49abc79b
Gerben Koopman <gerbennkoopman@gmail.com>
2025-05-21 14:29:53 +0200

    Removed comments that are unecessary

--- experiments/wrappers/cosmology.py
+++ experiments/wrappers/cosmology.py
@@ -9,12 +9,6 @@ from gatr.interface import (
 )
 
 
-# TODO: remove this
-# masses = inputs[:, :, [0]]  # (batchsize, objects, 1)
-# points = inputs[:, :, 1:4]  # (batchsize, objects, 3)
-# velocities = inputs[:, :, 4:7]  # (batchsize, objects, 3) # TODO: why 4,5,6 ???
-
-
 class Embedding(nn.Module):
     def __init__(self):
         super().__init__()
@@ -34,27 +28,31 @@ class CosmologyModel(nn.Module):
             in_mv_channels=main_model.out_dim,
             out_mv_channels=1,
             in_s_channels=main_model.out_dim,
-            out_s_channels=1
+            out_s_channels=1,
         )
 
     def forward(self, node_positions, **kwargs):
         node_features_mv = self.embedding_model(node_positions)  # Shape [bs*nodes, 16]
-        
+
         # Create scalar features with 16 channels as all zeros: WHY ALL ZEROS? => maybe worth to try all ones etc.
-        node_features_sc = torch.zeros(node_features_mv.shape[0], 16, device=node_features_mv.device)
-        
+        node_features_sc = torch.zeros(
+            node_features_mv.shape[0], 16, device=node_features_mv.device
+        )
+
         # Run the main model
-        mv_output, sc_output = self.main_model(node_features_mv, node_features_sc, node_positions, **kwargs)
-        
+        mv_output, sc_output = self.main_model(
+            node_features_mv, node_features_sc, node_positions, **kwargs
+        )
+
         # Process through EquiLinear prediction head
         mv_pred, sc_pred = self.pred_head(mv_output, sc_output)
-        
+
         # Extract translation components (bivector indices 4, 5, 6)
         velocity = mv_pred[..., [4, 5, 6]]
-        
-        # Reshape to remove the channel dimension 
+
+        # Reshape to remove the channel dimension
         velocity = velocity.squeeze(1)  # Result: [bs*nodes, 3]
-        
+
         return velocity
 
     def step(self, batch, prefix="train"):
