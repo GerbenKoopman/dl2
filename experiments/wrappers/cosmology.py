ab500c8b22e19417de922b63ce4e3145c1abc2e3
onderakacik <oakacik18@ku.edu.tr>
2025-05-21 15:51:45 +0200

    fixed pred head and extract point

--- experiments/wrappers/cosmology.py
+++ experiments/wrappers/cosmology.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
-from gatr.layers import EquiLinear
+from gatr.layers import (
+    EquiLinear, 
+    ScalarGatedNonlinearity)
 from gatr.interface import (
     embed_point,
     embed_scalar,
     embed_translation,
     embed_oriented_plane,
+    extract_point,
 )
 
 class Embedding(nn.Module):
@@ -14,7 +17,6 @@ class Embedding(nn.Module):
 
     def forward(self, pos):
         mv = embed_point(pos)
-
         return mv.unsqueeze(-2)
 
 
@@ -23,7 +25,18 @@ class CosmologyModel(nn.Module):
         super().__init__()
         self.main_model = main_model
         self.embedding_model = Embedding()
-        self.pred_head = EquiLinear(
+        
+        # First EquiLinear layer
+        self.pred_head1 = EquiLinear(
+            in_mv_channels=main_model.out_dim,
+            out_mv_channels=main_model.out_dim,
+            in_s_channels=main_model.out_dim,
+            out_s_channels=main_model.out_dim
+        )
+        # Nonlinearity
+        self.nonlin = ScalarGatedNonlinearity("gelu")
+        # Second EquiLinear layer
+        self.pred_head2 = EquiLinear(
             in_mv_channels=main_model.out_dim,
             out_mv_channels=1,
             in_s_channels=main_model.out_dim,
@@ -34,16 +47,21 @@ class CosmologyModel(nn.Module):
         node_features_mv = self.embedding_model(node_positions)  # Shape [bs*nodes, 16]
         
         # Create scalar features with 16 channels as all zeros: WHY ALL ZEROS? => maybe worth to try all ones etc.
-        node_features_sc = torch.zeros(node_features_mv.shape[0], 16, device=node_features_mv.device)
+        node_features_sc = torch.zeros(node_features_mv.shape[0], self.main_model.in_dim, device=node_features_mv.device)
         
         # Run the main model
         mv_output, sc_output = self.main_model(node_features_mv, node_features_sc, node_positions, **kwargs)
         
-        # Process through EquiLinear prediction head
-        mv_pred, sc_pred = self.pred_head(mv_output, sc_output)
+        # First layer
+        mv_hidden, sc_hidden = self.pred_head1(mv_output, sc_output)
+        # Nonlinearity
+        mv_hidden, sc_hidden = self.nonlin(mv_hidden, sc_hidden)
+        # Second layer
+        mv_pred, sc_pred = self.pred_head2(mv_hidden, sc_hidden)
         
         # Extract translation components (bivector indices 4, 5, 6)
-        velocity = mv_pred[..., [4, 5, 6]]
+        # velocity = mv_pred[..., [4, 5, 6]]
+        velocity = extract_point(mv_pred, divide_by_embedding_dim=True)
         
         # Reshape to remove the channel dimension 
         velocity = velocity.squeeze(1)  # Result: [bs*nodes, 3]
