08c6c1f2a83611a4cf9067b1171de2b4c8835f2f
Gerben Koopman <gerbennkoopman@gmail.com>
2025-05-26 14:23:21 +0200

    Added to parser as well

--- experiments/train_cosmology.py
+++ experiments/train_cosmology.py
@@ -29,7 +29,7 @@ def parse_args():
         "--size",
         type=str,
         default="small",
-        choices=["smallest", "smallest_mp", "small", "medium", "large"],
+        choices=["smallest", "smallest_mp", "small", "small_mp", "medium", "large"],
         help="Model size configuration",
     )
     parser.add_argument(
